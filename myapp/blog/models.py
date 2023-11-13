bafcfb3aca3fe008784700e3071fdea9787ce34b
Jounghyo Lee <tlfqj0617@naver.com>
2023-11-13 15:04:14 +0900

    refactor : post models 수정

--- myapp/blog/models.py
+++ myapp/blog/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
-    content = models.TextField()
+    content = models.TextField(max_length=2000)
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
